3a6a9c3da0e8090b06d4607127ce0fddf6b4b243
Alejandro Perez <perezalejandrodl14@gmail.com>
2024-11-13 05:42:49 -0600

    Cambio de BrowserRouter a HashRouter para resolver problemas de enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Idioma from './components/Idioma';
+import Main from './components/Main';
+import MainMenu from './components/MainMenu';
+import Menu from './components/Menu';
+import Bebida from './components/Bebida';
+import Comida from './components/Comida';
+import Horarios from './components/Horarios';
+import Eventos from './components/Eventos';
+import Contacto from './components/Contacto';
+import Reservacion from './components/Reservacion';
+import Opinion from './components/Opinion';
+import Encuesta from './components/Encuesta';
+import VideoEncuesta from './components/VideoEncuesta';
+import Publicidad from './components/Especiales';
+import Loader from './Loader';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula un retraso en la carga o espera de datos inicial
+    const timer = setTimeout(() => setIsLoading(false), 2000); // Tiempo ajustable
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Idioma />} />
+        <Route path="/Interactua" element={<Main />} />
+        <Route path="/Main" element={<MainMenu />} />
+        <Route path="/Menu" element={<Menu />} />
+        <Route path="/Bebida" element={<Bebida />} />
+        <Route path="/Comida" element={<Comida />} />
+        <Route path="/Horarios" element={<Horarios />} />
+        <Route path="/Eventos" element={<Eventos />} />
+        <Route path="/Contacto" element={<Contacto />} />
+        <Route path="/Reservacion" element={<Reservacion />} />
+        <Route path="/Encuesta" element={<Encuesta />} />
+        <Route path="/Opinion" element={<Opinion />} />
+        <Route path="/VideoEncuesta" element={<VideoEncuesta />} />
+        <Route path="/especiales" element={<Publicidad />} />
+      </Routes>
+    </Router>
   );
 }
 
