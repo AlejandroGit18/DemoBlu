57cd146fd7f30d2d7d3f592c6ba755a1f841abb1
Alejandro Perez <perezalejandrodl14@gmail.com>
2024-11-14 07:18:34 -0600

    intento 2  version ios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Idioma from './components/Idioma';
-import Main from './components/Main';
-import MainMenu from './components/MainMenu';
-import Menu from './components/Menu';
-import Bebida from './components/Bebida';
-import Comida from './components/Comida';
-import Horarios from './components/Horarios';
-import Eventos from './components/Eventos';
-import Contacto from './components/Contacto';
-import Reservacion from './components/Reservacion';
-import Opinion from './components/Opinion';
-import Encuesta from './components/Encuesta';
-import Publicidad from './components/Especiales';
+import React, { Suspense, useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Loader from './Loader';
+
+// Importación diferida de componentes para mejorar la carga
+const Idioma = React.lazy(() => import('./components/Idioma'));
+const Main = React.lazy(() => import('./components/Main'));
+const MainMenu = React.lazy(() => import('./components/MainMenu'));
+const Menu = React.lazy(() => import('./components/Menu'));
+const Bebida = React.lazy(() => import('./components/Bebida'));
+const Comida = React.lazy(() => import('./components/Comida'));
+const Horarios = React.lazy(() => import('./components/Horarios'));
+const Eventos = React.lazy(() => import('./components/Eventos'));
+const Contacto = React.lazy(() => import('./components/Contacto'));
+const Reservacion = React.lazy(() => import('./components/Reservacion'));
+const Opinion = React.lazy(() => import('./components/Opinion'));
+const Encuesta = React.lazy(() => import('./components/Encuesta'));
+const Publicidad = React.lazy(() => import('./components/Especiales'));
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Simula un retraso en la carga o espera de datos inicial
-    const timer = setTimeout(() => setIsLoading(false), 2000); // Tiempo ajustable
+    // Detectar iOS y ajustar el temporizador para mejorar el rendimiento en dispositivos iOS
+    const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    const timer = setTimeout(() => setIsLoading(false), isiOS ? 500 : 2000); // Reducir el tiempo en iOS
     return () => clearTimeout(timer);
   }, []);
 
@@ -30,21 +33,23 @@ function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Idioma />} />
-        <Route path="/Interactua" element={<Main />} />
-        <Route path="/Main" element={<MainMenu />} />
-        <Route path="/Menu" element={<Menu />} />
-        <Route path="/Bebida" element={<Bebida />} />
-        <Route path="/Comida" element={<Comida />} />
-        <Route path="/Horarios" element={<Horarios />} />
-        <Route path="/Eventos" element={<Eventos />} />
-        <Route path="/Contacto" element={<Contacto />} />
-        <Route path="/Reservacion" element={<Reservacion />} />
-        <Route path="/Encuesta" element={<Encuesta />} />
-        <Route path="/Opinion" element={<Opinion />} />
-        <Route path="/especiales" element={<Publicidad />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Idioma />} />
+          <Route path="/Interactua" element={<Main />} />
+          <Route path="/Main" element={<MainMenu />} />
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/Bebida" element={<Bebida />} />
+          <Route path="/Comida" element={<Comida />} />
+          <Route path="/Horarios" element={<Horarios />} />
+          <Route path="/Eventos" element={<Eventos />} />
+          <Route path="/Contacto" element={<Contacto />} />
+          <Route path="/Reservacion" element={<Reservacion />} />
+          <Route path="/Encuesta" element={<Encuesta />} />
+          <Route path="/Opinion" element={<Opinion />} />
+          <Route path="/especiales" element={<Publicidad />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
